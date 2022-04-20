baa79f88b1bf817ba9f9c26901c38d8218afc3b2
windy <windyijihan@gmail.com>
2022-04-20 13:38:30 +0700

    fixing bugs

--- src/handler.js
+++ src/handler.js
@@ -37,45 +37,54 @@ const addBookHandler = (request, h) => {
     books.push(newBook);
 
     const isSuccess = books.filter((book) => book.id === id).length > 0;
+    const isBookHasOwnProperty = books.hasOwnProperty('name');
+    const isReadPageMoreThanPage = readPage>pageCount;
 
-    if (isSuccess){
+    if(isReadPageMoreThanPage){
         const response = h.response({
-            status : 'success',
-            message : 'Buku berhasil ditambahkan',
-            data : {
-                bookId : id
-            }
+            status : "fail",
+            message : "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount"
         });
-        response.code(201);
+        response.code(400);
+        return response;
+    }
+    
+    if(isBookHasOwnProperty===false){
+        const response = h.response({
+            status : "fail",
+            message : "Gagal menambahkan buku. Mohon isi nama buku"
+        });
+        response.code(400);
         return response;
     }
     else{
-        if(name === ''){
+        if(isSuccess){
             const response = h.response({
-                status : "fail",
-                message : "Gagal mendambahkan buku. Mohon isi nama buku"
+                status : 'success',
+                message : 'Buku berhasil ditambahkan',
+                data : {
+                    bookId : id
+                }
             });
-            response.code(400);
-            return response;
-        }
-        else if(readPage > pageCount){
-            const response = h.response({
-                status : "fail",
-                message : "Gagal mendambahkan buku. readPage tidak boleh lebih besar dari pageCount"
-            });
-            response.code(400);
-            return response;
-        }
-        else{
-            const response = h.response({
-                status : "fail",
-                message : "Buku gagal ditambahkan"
-            });
-            response.code(500);
+            response.code(201);
             return response;
         }
     }
-
+    
+    
+    const response = h.response({
+        status : "fail",
+        message : "Buku gagal ditambahkan"
+    });
+    response.code(500);
+    return response;
 };
 
-module.exports = { addBookHandler };
+const getAllBooksHandler = () => ({
+    status : 'success',
+    data : {
+        books,
+    },
+});
+
+module.exports = { addBookHandler, getAllBooksHandler };
